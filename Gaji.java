b4a5b9ccf8be8a2d8e0cebdaec282ac6338333ef
SabdaPutraAribawa <aribawap082@gmail.com>
2024-12-08 00:48:27 +0700

    Update Gaji.java

--- Gaji.java
+++ Gaji.java
@@ -1,61 +1,47 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
-public class Hitung{
+public class Gaji{
+    public static int masaKerja;
 
-    public static ArrayList<Integer> data = new ArrayList<>();
-
-    public static double rataRata(ArrayList<Integer>data){
-        double total = 0;
-        for (int i = 0; i < data.size(); i++) {
-                total += data.get(i);
-            }
-        double rata = total / data.size();
-        return rata;
+    public static int gaji(int masaKerja){
+    if (masaKerja<5) {
+        return 5000000;
+    } else if (masaKerja>=5&&masaKerja<=10) {
+        return 7500000;
+    } else 
+    return 10000000;
     }
 
-    public static int nilaiMin (ArrayList<Integer>data){
-        int min = data.get(0);
-        for (int i = 0; i < data.size(); i++) {
-            if (data.get(i) < min) {
-                min = data.get(i);
-                }
-                }
-                return min;
+    public static int lembur(int masaKerja){
+        if (masaKerja<5) {
+            return 40000;
+        } else if (masaKerja>=5&&masaKerja<=10) {
+            return 50000;
+        } else 
+        return 75000;
     }
-    
-    public static int nilaiMax (ArrayList<Integer>data){
-        int max = data.get(0);
-        for (int i = 0; i < data.size(); i++) {
-            if (data.get(i) > max) {
-                max = data.get(i);
-                }
-                }
-                return max;
-    }
-
-
     public static void main(String[] args){
+
         Scanner input = new Scanner(System.in);
+        masaKerja = input.nextInt();
+        int jamLembur = input.nextInt();
+
         /* Pekerjaan anda mulai dari sini */
 
-        String jawab;
+        int gajiBulanan = gaji(masaKerja);
+        int lemburBulanan = lembur(masaKerja);
 
-        do {
-            data.add(input.nextInt());
+        lemburBulanan*=jamLembur;
+        gajiBulanan+=lemburBulanan;
 
-            jawab = input.next().trim().toUpperCase();
-            while (!jawab.equals("Y") && !jawab.equals("T")) {
-                jawab = input.next().trim().toUpperCase();
-            }
-        } while (jawab.equals("Y"));
-
-        System.out.println("Nilai minimal : "+ nilaiMin(data));
-        System.out.println("Nilai maksimal : "+ nilaiMax(data));
-        System.out.println("Nilai rata-rata : "+ rataRata(data));
-
+        System.out.println(gajiBulanan);
+        
         /* Pekerjaan anda berakhir sini */
 
+        System.out.println(Gaji.class.getDeclaredMethods().length);
+        System.out.println(Gaji.class.getDeclaredFields().length);
+        
         input.close();
     }
+    
 }
